66849050d78c4a979d1cb78b7f4ee12e85d3685b
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-09-07 11:49:30 -0300

    Requisito  1

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,1 +1,54 @@
-test('', () => {});
+import { render, screen, fireEvent } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../App';
+
+test('Deve renderizar os links de navegação corretamente', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
+  expect(screen.getByText('Home')).toBeInTheDocument();
+  expect(screen.getByText('About')).toBeInTheDocument();
+  expect(screen.getByText('Favorite Pokémon')).toBeInTheDocument();
+});
+
+test('Deve redirecionar para a página Home ao clicar no link Home', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
+
+  const homeLink = screen.getByText('Home');
+
+  fireEvent.click(homeLink);
+
+  expect(window.location.pathname).toBe('/');
+});
+
+test('Deve redirecionar para a página About ao clicar no link About', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
+
+  const aboutLink = screen.getByText('About');
+
+  fireEvent.click(aboutLink);
+  expect(window.location.pathname).toBe('/about');
+});
+
+test('Deve redirecionar para a página Favorite Pokémon ao clicar no link Favorite Pokémon', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
+
+  const favoriteLink = screen.getByText('Favorite Pokémon');
+
+  fireEvent.click(favoriteLink);
+  expect(window.location.pathname).toBe('/favorites');
+});
