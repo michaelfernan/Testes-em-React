c96a9d8cb4b12adfeef5673527eb26138c06f339
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-09-07 11:57:01 -0300

    Requisito 2

--- src/tests/About.test.tsx
+++ src/tests/About.test.tsx
@@ -1,1 +1,15 @@
-test('t', () => {});
+import { render, screen } from '@testing-library/react';
+import About from '../pages/About/About';
+
+test('Deve exibir informações sobre a Pokédex', () => {
+  render(<About />);
+
+  const heading = screen.getByRole('heading', { name: /About Pokédex/i });
+  expect(heading).toBeInTheDocument();
+
+  const paragraphs = screen.getAllByText(/This application simulates a Pokédex/i);
+  expect(paragraphs.length).toBeGreaterThan(0);
+
+  const pokedexImage = screen.getByAltText('Pokédex');
+  expect(pokedexImage).toBeInTheDocument();
+});
