45aa4977fa600a0d68b39e0f17db413c3d9d20cc
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-09-08 15:16:46 -0300

    Requisito 2

--- src/tests/About.test.tsx
+++ src/tests/About.test.tsx
@@ -1,15 +1,30 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import About from '../pages/About/About';
 
-test('Deve exibir informações sobre a Pokédex', () => {
-  render(<About />);
+describe('<About />', () => {
+  let container: Element;
 
-  const heading = screen.getByRole('heading', { name: /About Pokédex/i });
-  expect(heading).toBeInTheDocument();
+  beforeEach(() => {
+    const renderResult = render(<About />);
+    container = renderResult.container;
+  });
 
-  const paragraphs = screen.getAllByText(/This application simulates a Pokédex/i);
-  expect(paragraphs.length).toBeGreaterThan(0);
+  it('deve exibir um h2 com texto Sobre Pokédex', () => {
+    const heading = screen.getByRole('heading', { name: /About Pokédex/i });
+    expect(heading).toBeInTheDocument();
+    expect(heading.tagName).toBe('H2');
+  });
 
-  const pokedexImage = screen.getByAltText('Pokédex');
-  expect(pokedexImage).toBeInTheDocument();
+  it('deve exibir dois parágrafos sobre a Pokédex', () => {
+    const paragraphs = container.querySelectorAll('p');
+    expect(paragraphs.length).toBe(2);
+  });
+
+  it('deve exibir a imagem correta de um Pokédex', () => {
+    const image = screen.getByRole('img', { name: /Pokédex/i });
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
 });
