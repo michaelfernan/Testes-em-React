d6465e2697c6fb41a84e2ce0e993d6a7bef0077a
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-09-08 16:49:10 -0300

    Requisito 4

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,20 @@
-test('', () => {});
+// src/tests/NotFound.test.tsx
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound/NotFound';
+
+describe('Componente NotFound', () => {
+  it('deve exibir um cabeçalho h2 com o texto "Page requested not found"', () => {
+    render(<NotFound />);
+    const elementoTitulo = screen.getByRole('heading', { level: 2 });
+    expect(elementoTitulo).toBeInTheDocument();
+    expect(elementoTitulo).toHaveTextContent('Page requested not found');
+  });
+
+  it('deve exibir uma imagem com o texto alternativo correto', () => {
+    render(<NotFound />);
+    const elementoImagem = screen.getByAltText("Clefairy pushing buttons randomly with text I have no idea what i'm doing");
+    expect(elementoImagem).toBeInTheDocument();
+  });
+});
