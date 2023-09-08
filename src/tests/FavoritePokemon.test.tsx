3385ffedc7b31164ffde91d247f0136e87beaba3
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-09-08 14:52:17 -0300

    Requesito 3

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,62 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import FavoritePokemon from '../pages/FavoritePokemon/FavoritePokemon';
+import { PokemonType, AverageWeightType, FoundAtType } from '../types/index';
+
+describe('Componente Pokémon favorito', () => {
+  it('"Nenhum Pokémon favorito encontrado" quando nenhum Pokémon for favorito', () => {
+    render(
+      <MemoryRouter>
+        <FavoritePokemon pokemonList={ [] } />
+      </MemoryRouter>,
+    );
+    const message = screen.getByText(/No favorite Pokémon found/i);
+    expect(message).toBeInTheDocument();
+  });
+
+  it('Deve Exebir apenas os Pokemon Favoritos', () => {
+    const averageWeight: AverageWeightType = {
+      measurementUnit: 'kg',
+      value: '6.0',
+    };
+
+    const foundAt: FoundAtType[] = [
+    ];
+
+    const fakePokemonList: PokemonType[] = [
+      {
+        id: 1,
+        name: 'Pikachu',
+        type: 'Electric',
+        averageWeight,
+        foundAt,
+        image: 'pikachu.jpg',
+        moreInfo: 'https://example.com/pikachu',
+        summary: 'Electric Pokémon',
+      },
+      {
+        id: 2,
+        name: 'Bulbasaur',
+        type: 'Grass',
+        averageWeight,
+        foundAt,
+        image: 'bulbasaur.jpg',
+        moreInfo: 'https://example.com/bulbasaur',
+        summary: 'Grass Pokémon',
+      },
+    ];
+
+    render(
+      <MemoryRouter>
+        <FavoritePokemon pokemonList={ fakePokemonList } />
+      </MemoryRouter>,
+    );
+
+    fakePokemonList.forEach((pokemon) => {
+      expect(screen.getByText(new RegExp(pokemon.name, 'i'))).toBeInTheDocument();
+    });
+
+    expect(screen.queryByText(/Charmander/i)).toBeNull();
+  });
+});
